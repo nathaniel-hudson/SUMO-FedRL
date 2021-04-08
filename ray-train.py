0b1ee5f2fe9ffadcb50867cc7ffaa81e68bb4b53
Nathaniel Hudson <nchudson88@gmail.com>
2021-04-08 16:25:21 -0400

    path compat

--- ray-train.py
+++ ray-train.py
@@ -8,18 +8,32 @@ https://github.com/LucasAlegre/sumo-rl/blob/master/experiments/a3c_4x4grid.py
 Ray RlLib agent training example.
 https://github.com/ray-project/ray/blob/master/rllib/examples/custom_train_fn.py
 """
+import os
 import pandas as pd
 import fluri.trainer.ray.fed_agent as fedrl
 import fluri.trainer.ray.multi_agent as marl
 import fluri.trainer.ray.single_agent as sarl
 
+from datetime import datetime
 from os.path import join
+
+SINGLEAGENT = 1
+MULTIAGENT = 2
+FEDAGENT = 3
+
+
+def date_dir() -> str:
+    date = datetime.now()
+    return f"{date.year}-{date.month}-{date.day}_{date.hour}.{date.minute}"
+
+def save_df(df: pd.DataFrame, path: str) -> None:
+    path_dir = join(*path.split(os.sep)[:-1])
+    if not os.path.exists(path_dir):
+        os.makedirs(path_dir)
+    df.to_csv(path)
 
 
 if __name__ == "__main__":
-    SINGLEAGENT = 1
-    MULTIAGENT = 2
-    FEDAGENT = 3
 
     prompt = "Enter [1] single-agent, [2] multi-agent, or [3] fed-agent: "
     kind = input(prompt)
@@ -32,15 +46,19 @@ if __name__ == "__main__":
     if kind == SINGLEAGENT:
         results = sarl.train(n_rounds)
         results_df = pd.DataFrame.from_dict(results)
-        results_df.to_csv("sarl_data.csv")
+        filename = "sarl_data.csv"
 
     elif kind == MULTIAGENT:
         results = marl.train(n_rounds)
         results_df = pd.DataFrame.from_dict(results)
-        results_df.to_csv(join("", "", "marl_data.csv"))
+        filename = "marl_data.csv"
 
     elif kind == FEDAGENT:
         fed_step = int(input("Enter how many rounds before FedAvg: "))
         results = fedrl.train(n_rounds, fed_step)
         results_df = pd.DataFrame.from_dict(results)
-        results_df.to_csv("fedrl_data.csv")
+        filename = "fedrl_data.csv"
+
+        
+    path = join("out", "models", date_dir(), filename)
+    save_df(results_df, path)
